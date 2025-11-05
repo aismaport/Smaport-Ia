aacbda75566cea73cbe0cbab031fe11719940ec0
aismaport <aismaport@gmail.com>
2025-11-05 20:34:07 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,9 @@
 import os
 from fastapi import FastAPI, Depends, HTTPException, status, Body
+from pydantic import BaseModel, EmailStr
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from . import models, schemas, auth
